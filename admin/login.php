ea9c64e603dc38757daf656cc3b526eca303839f
DecodeGear <dev.nnw@gmail.com>
2024-11-30 10:43:39 -0300

    login-footer

--- admin/login.php
+++ admin/login.php
@@ -79,4 +79,4 @@ if ($usuario && password_verify($senha, $usuario['senha'])) {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
-<?php include "./includes/footer.php" ?>
+<?php include '../includes/footer.php'; ?>
