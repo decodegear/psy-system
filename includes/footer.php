5a5118a597786886204301b1cbb437c510922af6
Daniel Galvão <dev.nnw@gmail.com>
2025-01-03 15:54:41 -0300

    ..

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 <footer>
-        <p>&copy; 2024 Sistema de Gestão. Todos os direitos reservados. | V0.88</p>
+        <p>&copy; 2024 Sistema de Gestão. Todos os direitos reservados. | V0.89</p>
     </footer>
     <script src="../assets/js/scripts.js"></script>
 </body>
