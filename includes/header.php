4f29ce6054d2bc13429352ddbcda94dbfcce3308
Daniel Galvão <dev.nnw@gmail.com>
2025-01-03 15:33:52 -0300

    organizando

--- includes/header.php
+++ includes/header.php
@@ -14,7 +14,7 @@ include 'db_connect.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gestão Psicológica</title>
+    <title>Alexandre Batista</title>
     <!-- Link para o Bootstrap -->
     <link rel="stylesheet" href="../assets/css/style.css"> <!-- Link para o estilo CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
