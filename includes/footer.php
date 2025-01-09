c4af1acf0268f95439630cde6d61b543b471f7c8
Daniel Galvão <dev.nnw@gmail.com>
2025-01-09 16:16:37 -0300

    V0.90 color

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 <footer>
-<div ><p>&copy; 2024 Sistema de Gestão. Todos os direitos reservados. | V0.89</p></div>
+<div ><p>&copy; 2024 Sistema de Gestão. Todos os direitos reservados. | V0.90</p></div>
     </footer>
     <script src="../assets/js/scripts.js"></script>
 </body>
